2665fda189058342bc3fbe1624ae6c47d273bc6f
Thomas Thelen <tommythelen@gmail.com>
2020-12-17 21:40:00 -0800

    Add unit tests for re-loading the generated RDF

--- sdtlconverter/tests/test_v1converter.py
+++ sdtlconverter/tests/test_v1converter.py
@@ -1,2 +1,22 @@
-def test_init():
-    assert 1==1
+from sdtlconverter.ConverterV1 import ConverterV1
+import rdflib
+import os
+
+
+def test_write_read():
+    """
+    Tests that the RDF produce can be read back by rdflib.
+
+    """
+
+    converter = ConverterV1("./integrated/single_load/load.json")
+    converter.convert_sdtl_to_rdf()
+
+
+    try:
+        with open("test_jsonld.jsonld") as temp_file:
+            rdflib.Graph().parse(data=temp_file.read(), format="json-ld")
+    except Exception:
+        assert False
+    finally:
+        os.remove("test_jsonld.jsonld")
